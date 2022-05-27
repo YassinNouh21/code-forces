240cebd6ccb2b93da821034d0817a6b04d54221a
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-05-28 00:45:41 +0200

    edit test number I forgot it

--- way_too_long_words.py
+++ way_too_long_words.py
@@ -1,11 +1,13 @@
 
-word = str(input())
+test_number = int (input())
 
 def split_word(word):
     return word[1:-1]
 
-if len(word) <= 10:
-    print(word)
-else:
-    print(f'{word[0]}{len(split_word(word))}{word[-1]}')
+for i in range(test_number):
+    word = str(input())
+    if len(word) <= 10:
+        print(word)
+    else:
+        print(f'{word[0]}{len(split_word(word))}{word[-1]}')
 
