e5e07d27dc844182ae0d410f8b28767e90d07407
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-05-28 00:33:57 +0200

    update base case 2

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-num = int(input('enter the number'))
-if (num % 2) == 0:
-    print('YES')
-else:
+num = int(input())
+if num <= 2:
     print('NO')
+else:
+    if (num % 2) == 0:
+        print('YES')
+    else:
+        print('NO') 
